d4946aba4a6656ecdeef54247d7ffd15fab27cbb
vitaliy_punko <sapoghnik23@gmail.com>
2025-03-02 14:22:16 +0300

    set custom jwt token converter to check token

--- src/main/java/vpunko/spotify/security/config/SecurityConfig.java
+++ src/main/java/vpunko/spotify/security/config/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -22,8 +24,9 @@ public class SecurityConfig {
                 .csrf((csrf) -> csrf.disable())
                 .cors((cors) -> cors.disable())
                 .authorizeHttpRequests((requests) -> requests
-//                        .requestMatchers("/home", "/testPostArticle").permitAll()
-                        .anyRequest().authenticated()
+                                .requestMatchers(
+                                        "/favicon.ico", "/error", "/logout", "/login").permitAll()
+                                .anyRequest().authenticated()
                 )
                 .formLogin((formLogin) ->
                         formLogin
@@ -32,7 +35,24 @@ public class SecurityConfig {
 
                 )
                 .oauth2Login(withDefaults());
+
+        http.oauth2ResourceServer(oauth2rs -> oauth2rs
+                .jwt((jwt) ->
+                        jwt
+                                .jwtAuthenticationConverter(jwtAuthenticationConverter())
+                )
+        );
         return http.build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter converter = new JwtGrantedAuthoritiesConverter();
+        converter.setAuthoritiesClaimName("roles");
+        converter.setAuthorityPrefix("ROLE_");
+        JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
+        jwtConverter.setJwtGrantedAuthoritiesConverter(converter);
+        return jwtConverter;
     }
 
 
