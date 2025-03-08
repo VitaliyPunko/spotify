ab2d54ce2f5bfb4ffb4db67d5100181a2ef7e105
vitaliy_punko <sapoghnik23@gmail.com>
2025-03-09 00:53:25 +0300

    add a status badge

--- src/main/java/vpunko/spotify/core/jobs/JobController.java
+++ src/main/java/vpunko/spotify/core/jobs/JobController.java
@@ -3,7 +3,6 @@ package vpunko.spotify.core.jobs;
 import org.jobrunr.jobs.JobId;
 import org.jobrunr.jobs.annotations.Job;
 import org.jobrunr.jobs.annotations.Recurring;
-import org.jobrunr.scheduling.BackgroundJob;
 import org.jobrunr.scheduling.JobScheduler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,14 +23,14 @@ public class JobController {
         final JobId enqueuedJobId = jobScheduler.enqueue(() -> {
             String s = "Hello " + name;
             System.out.println(s);
-        } );
+        });
 
         return "Job Enqueued: " + enqueuedJobId.toString();
     }
 
     //todo: try https://www.jobrunr.io/en/documentation/background-methods/
     // MyJobRequestHandler and run method
-    @Recurring(id = "my-recurring-job", cron = "*/1 * * * *")
+    @Recurring(id = "my-recurring-job", cron = "*/100 * * * *")
     @Job(name = "My recurring job")
     public void executeSampleJob() {
         System.out.println("Cron job");
